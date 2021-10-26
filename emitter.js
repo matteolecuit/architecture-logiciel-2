073d990980116ffa7d33a9bc12abc2572acf8fe9
aelmaknati <abdellatif.elmaknati@dakilab.fr>
2021-10-26 12:16:06 +0200

    Init commit

--- emitter.js
+++ emitter.js
@@ -1,18 +1,23 @@
 const { fork } = require('child_process');
+const accounts  = require('./accounts.json');
 const {
     v4: uuidv4,
 } = require('uuid');
+
+
 let delay = process.argv[2] || 100
 let agent = fork('agent');
 async function start(){
     setInterval(async () => {
         let isCorrupted = Math.random() < 0.1;
+        let accountIndex = Math.floor(Math.random() * 99)
         let transaction = {
+            account_num :accounts[accountIndex].account_id,
             timestamp : Date.now(),
             transactionId : uuidv4(),
             amount : isCorrupted ? -1 : Math.floor(Math.random() * (10000 +2 +1)) -2
         }
-        console.log("Transaction "+transaction.transactionId+" ("+transaction.amount+"€) envoyée");
+        console.log(`[EMITTER] Transaction n°${transaction.transactionId} (${transaction.amount}€) envoyée`);
 
         // Appel de l'agent pour le traitement de la transaction
         agent.send(JSON.stringify(transaction))
