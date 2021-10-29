1fe8a55b31914c19b4fa3254cc23692dcf0a28bc
Matteo Lecuit <matteo@lecuit.fr>
2021-10-29 14:52:04 +0200

    working mq

--- emitter.js
+++ emitter.js
@@ -6,42 +6,38 @@ const amqp = require("amqplib/callback_api");
 let delay = process.argv[2] || 100;
 let agent = fork("agent");
 async function start() {
-  setInterval(async () => {
-    let isCorrupted = Math.random() < 0.1;
-    let accountIndex = Math.floor(Math.random() * 99);
-    let transaction = {
-      account_num: accounts[accountIndex].account_id,
-      timestamp: Date.now(),
-      transactionId: uuidv4(),
-      amount: isCorrupted
-        ? -1
-        : Math.floor(Math.random() * (10000 + 2 + 1)) - 2,
-    };
-    console.log(
-      `[EMITTER] Transaction n°${transaction.transactionId} (${transaction.amount}€) envoyée`
-    );
-
-    // Appel de l'agent pour le traitement de la transaction
-    agent.send(JSON.stringify(transaction));
-    sendMessage(JSON.stringify(transaction));
-  }, delay);
-}
-
-start();
-
-const sendMessage = (transaction) => {
   amqp.connect("amqp://guest:guest@127.0.0.1:5672", function (err, conn) {
     if (err) {
       console.log("error", err);
     } else {
-      conn.createChannel(function (err, ch) {
-        const ex = "transactions";
-        for (let postIndex = 1; postIndex < 15; postIndex++) {
-          ch.assertExchange(ex, "fanout", { durable: false });
-          ch.publish(ex, "", new Buffer(transaction));
-          console.log(" [x] Sent %s", transaction);
-        }
-      });
+      setInterval(async () => {
+        let isCorrupted = Math.random() < 0.1;
+        let accountIndex = Math.floor(Math.random() * 99);
+        let transaction = {
+          account_num: accounts[accountIndex].account_id,
+          timestamp: Date.now(),
+          transactionId: uuidv4(),
+          amount: isCorrupted
+            ? -1
+            : Math.floor(Math.random() * (10000 + 2 + 1)) - 2,
+        };
+        agent.send(JSON.stringify(transaction));
+        await sendMessage(conn, JSON.stringify(transaction)).catch(console.log);
+      }, delay);
     }
   });
+}
+
+start();
+
+const sendMessage = async (conn, transaction) => {
+  try {
+    await conn.createChannel(async function (err, ch) {
+      const ex = "transactions";
+      await ch.sendToQueue(ex, Buffer.from(transaction), { persistent: true });
+      console.log(" [x] Sent %s", transaction);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
