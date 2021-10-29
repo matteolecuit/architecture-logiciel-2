25f4ea5e96dda48e556c65d3d0c2c2a7af1b5776
Matteo Lecuit <matteo@lecuit.fr>
2021-10-29 15:03:37 +0200

    subscribe

--- agent.js
+++ agent.js
@@ -1,7 +1,18 @@
+const amqp = require("amqplib/callback_api");
 
-process.on("message" , (message) => {
-    let transaction = JSON.parse(message);
-    setTimeout(() => {
-        console.log(`[AGENT] Transaction n° ${transaction.transactionId} traitée avec succès`)
-    } , 3000)
-})
+process.on("message", (message) => {
+  amqp.connect("amqp://guest:guest@127.0.0.1:5672", function (err, conn) {
+    conn.createChannel(async function (err, ch) {
+      try {
+        const ex = "transactions";
+        setTimeout(() => {
+          ch.consume(ex, (msg) => {
+            console.log("Message received: " + msg.content);
+          });
+        }, 3000);
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  });
+});
